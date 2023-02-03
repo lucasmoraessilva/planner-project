f265cc2cdda092db820988219ba47275b1fea78f
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-03 10:26:47 -0300

    feat: criação do schema de usuário

---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,40 @@
+import { Schema, Types } from "mongoose";
+
+export const userSchema = new Schema({
+    _id: {
+        type: Types.ObjectId,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    birthDate: {
+        type: Date,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+    }
+});
