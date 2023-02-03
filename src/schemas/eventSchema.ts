c7978b0982592af1c1ec9cb09906cf110caba329
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-03 10:43:19 -0300

    feat: criação do schema de eventos

---
+++ src/schemas/eventSchema.ts
@@ -0,0 +1,25 @@
+import { Schema, Types } from "mongoose";
+
+export const eventSchema = new Schema({
+    _id: {
+        type: Types.ObjectId,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    dateTime: {
+        type: Date,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+});
