9bedf4749e2a49a981a831fe19a728beefe657cd
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-04 20:25:18 -0300

    feat: criação do middleware de validação de eventos

---
+++ src/middlewares/eventValidationMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { eventValidator } from "../validators/eventValidator";
+
+export function eventValidationMiddleware(request: Request, response: Response, next: NextFunction) {
+    const validationErrors = validateEventProperties(eventValidator, request.body);
+
+    if(validationErrors.length){
+        return response.status(400).json({
+            errors: validationErrors
+        });
+    }
+
+    next();
+}
+
+function validateEventProperties(validator: any, objectToBeValidated: any){
+    let errors: string[] = [];
+
+    Object.keys(validator).forEach(key => {
+        const validationError = validator[key](objectToBeValidated[key]);
+
+        if(validationError)
+            errors.push(validationError);
+    });
+
+    return errors;
+}
