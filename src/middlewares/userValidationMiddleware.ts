5084cb13d8b8f02236188268d1e3d999c63635c1
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-04 15:09:57 -0300

    feat: criação do middleware de validação dos dados do usuário que vem pelo body da requisição HTTP

---
+++ src/middlewares/userValidationMiddleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { userValidator } from "../validators/userValidator";
+
+export function userValidationMiddleware(request: Request, response: Response, next: NextFunction) {
+    const validationErrors = validateUserProperties(userValidator, request.body);
+
+    if(validationErrors.length){
+        return response.status(400).json({
+            errors: validationErrors
+        });
+    }
+
+    next();
+}
+
+function validateUserProperties(validator: any, objectToBeValidated: any){
+    let errors: string[] = [];
+
+    Object.keys(validator).forEach(key => {
+        let validationError;
+        key !== 'confirmPassword'
+            ? validationError = validator[key](objectToBeValidated[key])
+            : validationError = validator[key](objectToBeValidated[key], objectToBeValidated.password);
+
+        if(validationError)
+            errors.push(validationError);
+    });
+
+    return errors;
+}
