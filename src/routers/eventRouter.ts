e217a2694bf34a5b4dc74c6bc39922996179506d
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-05 17:35:19 -0300

    feat: criação da rota para pegar dados de um evento através do id

--- src/routers/eventRouter.ts
+++ src/routers/eventRouter.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { EventController } from "../controllers/EventController";
-import { eventValidationMiddleware, userIdValidationMiddleware } from "../middlewares/eventValidationMiddleware";
+import { eventIdValidationMiddleware, eventValidationMiddleware, userIdValidationMiddleware } from "../middlewares/eventValidationMiddleware";
 import { MongoEventRepository } from "../repositories/implementations/MongoEventRepository";
 
 const eventRouter = Router({caseSensitive: true});
@@ -8,5 +8,6 @@ const eventController = new EventController(new MongoEventRepository());
 
 eventRouter.post('/', eventValidationMiddleware, eventController.create);
 eventRouter.get('/:userId', userIdValidationMiddleware, eventController.getAll);
+eventRouter.get('/:_id', eventIdValidationMiddleware, eventController.getById);
 
 export default eventRouter;
