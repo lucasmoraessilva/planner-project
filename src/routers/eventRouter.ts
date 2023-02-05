53889327da755328cf97d7d5b3d7f10fdf43208d
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-04 23:46:49 -0300

    feat: adição de rota para pegar todos os eventos de um usuário através de seu id

--- src/routers/eventRouter.ts
+++ src/routers/eventRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { EventController } from "../controllers/EventController";
-import { eventValidationMiddleware } from "../middlewares/eventValidationMiddleware";
+import { eventValidationMiddleware, userIdValidationMiddleware } from "../middlewares/eventValidationMiddleware";
 import { MongoEventRepository } from "../repositories/implementations/MongoEventRepository";
 
 const eventRouter = Router({caseSensitive: true});
 const eventController = new EventController(new MongoEventRepository());
 
 eventRouter.post('/', eventValidationMiddleware, eventController.create);
+eventRouter.get('/:userId', userIdValidationMiddleware, eventController.getAll);
 
 export default eventRouter;
