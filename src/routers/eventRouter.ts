4b6c1688152cb6b724efaa1b15277848c0edfd37
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-06 14:36:46 -0300

    ref: atualização da rota GET /events

--- src/routers/eventRouter.ts
+++ src/routers/eventRouter.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { EventController } from "../controllers/EventController";
-import { eventIdValidationMiddleware, eventValidationMiddleware, userIdValidationMiddleware } from "../middlewares/eventValidationMiddleware";
+import { eventIdValidationMiddleware, eventValidationMiddleware } from "../middlewares/eventValidationMiddleware";
 import { MongoEventRepository } from "../repositories/implementations/MongoEventRepository";
 
 const eventRouter = Router({caseSensitive: true});
 const eventController = new EventController(new MongoEventRepository());
 
 eventRouter.post('/', eventValidationMiddleware, eventController.create);
-eventRouter.get('/:userId', userIdValidationMiddleware, eventController.getAll);
+eventRouter.get('/', eventController.getAll);
 eventRouter.get('/:_id', eventIdValidationMiddleware, eventController.getById);
 eventRouter.delete('/:_id', eventIdValidationMiddleware, eventController.delete);
 
