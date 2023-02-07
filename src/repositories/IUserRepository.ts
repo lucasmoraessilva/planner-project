53117c26ec2f8a9ceada27aea890be15a174b9d8
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-02-06 23:59:27 -0300

    ref: alteração na definição do método signIn

--- src/repositories/IUserRepository.ts
+++ src/repositories/IUserRepository.ts
@@ -2,6 +2,6 @@ import { User } from "../entities/User";
 
 export interface IUserRepository {
     signUp(user: User): Promise<void>;
-    signIn(email: string, password: string): Promise<boolean>;
+    signIn(email: string, password: string): Promise<User>;
     updateAllFields(user: User): Promise<void>;
 }
